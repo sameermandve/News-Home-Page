3a326ccc1f0a02a5ba16aa06d9c8f75f5c6af386
Sameer Mandve <mandvesameer@gmail.com>
2024-06-22 18:35:25 +0530

    Fourth Commit

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const menuBtn = document.querySelector("#icon-menu");
+const closeBtn = document.querySelector("#icon-menu-close");
+const nav = document.querySelector("#nav-menu");
 
+menuBtn.addEventListener("click", () => {
+  const body = document.getElementsByTagName("body");
+  const behindMenu = document.querySelector("#wrapper");
+
+  nav.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+  nav.classList.remove("show");
+});
